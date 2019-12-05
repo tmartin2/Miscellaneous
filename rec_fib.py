459f432421486082d7b5c9382ec7e0e84b56ed34
Trevor Martin <astrabrauer@Trevors-MacBook-Pro.local>
2019-12-05 13:45:04 -0500

    recursive

---
+++ rec_fib.py
@@ -0,0 +1,16 @@
+'''
+My implementation of the fibonacci algorithm
+using recursion
+'''
+
+
+def fibonacci(number):
+    if number == 0:
+        return 0
+    elif number == 1:
+        return 1
+    else:
+        return fibonacci(number - 1) + fibonacci(number - 2) 
+
+print(fibonacci(4))
+print(fibonacci(6))
