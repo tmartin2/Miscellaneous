61050a4ffebfd1b12ca396592134c8efa98a71e3
Trevor Martin <43895813+tmartin2@users.noreply.github.com>
2020-02-03 15:08:29 -0500

    Update gradient-descent.py

--- WT-2019/gradient-descent.py
+++ WT-2019/gradient-descent.py
@@ -2,7 +2,9 @@
 
 A NumPy implementation of the gradient descent algorithm                                                                          
 
-Author: Trevor Martin                                                                                                              Date: 11 January 2020                                                                                                              """
+Author: Trevor Martin                                                                                                              
+Date: 11 January 2020                                                                                                              
+"""
 import numpy as np
 import sympy as sym
 import matplotlib.pyplot as plt
