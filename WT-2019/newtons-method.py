43abed06dbdbb8d95f54622406bb771b09f4a1e1
Trevor Martin <43895813+tmartin2@users.noreply.github.com>
2020-02-03 15:09:03 -0500

    Update newtons-method.py

--- WT-2019/newtons-method.py
+++ WT-2019/newtons-method.py
@@ -1,7 +1,9 @@
 """Gradient Descent                                                                                                                 
 A NumPy implementation of the gradient descent algorithm                                                                           
                                                                                                                   
-Author: Trevor Martin                                                                                                              Date: 11 January 2020                                                                                                              """
+Author: Trevor Martin                                                                                                              
+Date: 11 January 2020                                                                                                              
+"""
 import numpy as np
 import sympy as sym
 import matplotlib.pyplot as plt
