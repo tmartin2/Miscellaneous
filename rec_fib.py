d2e7ab5739d4122f502404a63eeddb68f964a374
Trevor Martin <43895813+tmartin2@users.noreply.github.com>
2020-04-17 21:16:22 -0400

    Update rec_fib.py

--- rec_fib.py
+++ rec_fib.py
@@ -6,12 +6,9 @@ using recursion
 __author__ = "Trevor Martin"
 
 def fibonacci(number):
-    if number == 0:
-        return 0
-    elif number == 1:
-        return 1
-    else:
-        return fibonacci(number - 1) + fibonacci(number - 2) 
+    if number == 0 or number == 1:
+        return number
+    return fibonacci(number - 1) + fibonacci(number - 2) 
 
-print(fibonacci(4))
-print(fibonacci(6))
+if __name__ == '__main__':
+    print(fibonacci(input("Find the n-th fibonacci number: "))
