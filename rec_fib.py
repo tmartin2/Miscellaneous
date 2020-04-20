b07862454c79f6b700ad07bd4c123980e76c7cb8
Trevor Martin <astrabrauer@Trevors-MBP.fios-router.home>
2020-04-20 12:57:38 -0400

    all

--- rec_fib.py
+++ rec_fib.py
@@ -8,7 +8,7 @@ __author__ = "Trevor Martin"
 def fibonacci(number):
     if number == 0 or number == 1:
         return number
-    return fibonacci(number - 1) + fibonacci(number - 2) 
+    return fibonacci(number-1) + fibonacci(number - 2) 
 
 if __name__ == '__main__':
-    print(fibonacci(input("Find the n-th fibonacci number: "))
+    print(fibonacci(int(input("Find the n-th fibonacci number: "))))
